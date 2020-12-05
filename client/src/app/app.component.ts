261f9aac00d3d0fbcafc0c26f61e15a2454d8342
Rene Aguilera <raguilera@thresholdcommunications.com>
2020-12-04 22:12:43 -0600

    Adding Angular to the project

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  title = 'DatingAppp';
+  users: any;
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get("https://localhost:5001/api/users").subscribe(res => {
+      console.log(res)
+      this.users = res;
+    }, error => {
+      console.log(error);
+    })
+  }
+
+
 }
